acc3816d94f7d8366066e0f919bfb7906ccbf3fc
Konstantinos Pilpilidis <kpilpilidis@gmail.com>
2026-09-25 13:14:06 +0200

    style(legal): use node namespace imports

--- apps/backend/src/modules/legal/legal.service.ts
+++ apps/backend/src/modules/legal/legal.service.ts
@@ -1,6 +1,6 @@
 import crypto from "node:crypto";
-import fs from "node:fs";
-import path from "node:path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import { HttpError } from "../../utils/http.js";
 import { insertAudit } from "../common/audit.util.js";
 import {
